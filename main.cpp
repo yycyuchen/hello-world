c4f54da6d123cca4bc436b64e36a6b5214a4dff7
yycyuchen <46766273+yycyuchen@users.noreply.github.com>
2019-01-16 19:01:32 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Yuchen Yao
+// Description: Hello World in C++
+//============================================================
+
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+int main()
+{
+	string name1="your name1";
+	hello1Function(name1);
+	string name2="your name2";
+	hello2Function(name2);
+	return 0;
+}
+
